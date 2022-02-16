2a39c07f5678011a86e3525c1bdb6acf43149a85
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-02-16 09:44:14 -0600

    removed vuf, added images, add line rendering scene

--- ar-playground/Assets/TestSpace/Scripts/TrackedImageInfoManager.cs
+++ ar-playground/Assets/TestSpace/Scripts/TrackedImageInfoManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
@@ -17,6 +18,9 @@ public class TrackedImageInfoManager : MonoBehaviour
 
   [Tooltip("If an image is detected but no source texture can be found, this texture is used instead.")]
   [SerializeField] private Texture2D m_DefaultTexture;
+  [SerializeField] private LineRenderer _line;
+
+  private List<Vector3> points;
 
   public Texture2D defaultTexture
   {
@@ -25,49 +29,53 @@ public class TrackedImageInfoManager : MonoBehaviour
   }
 
   private ARTrackedImageManager m_TrackedImageManager;
-  
-  
-  void Awake()
+
+  private void Awake()
   {
+    points = new List<Vector3>();
     m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
   }
-  
-  void OnEnable()
+
+  private void OnEnable()
   {
     m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
   }
-  
-  void OnDisable()
+
+  private void OnDisable()
   {
     m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
   }
-  
-  void UpdateInfo(ARTrackedImage trackedImage)
+
+  private void UpdateInfo(ARTrackedImage trackedImage)
   {
     // Set canvas camera
     var canvas = trackedImage.GetComponentInChildren<Canvas>();
     canvas.worldCamera = worldSpaceCanvasCamera;
-  
-    // Update information about the tracked image
+
+
+    // Update information about the tracked image   
     var text = canvas.GetComponentInChildren<Text>();
     text.text =
-      $"{trackedImage.referenceImage.name}\ntrackingState: "
-      + $"{trackedImage.trackingState}\nGUID: "
-      + $"{trackedImage.referenceImage.guid}\nReference size: "
-      + $"{trackedImage.referenceImage.size * 100f} cm\nDetected size: "
-      + $"{trackedImage.size * 100f} cm";
-  
+      $"{trackedImage.referenceImage.name}"
+      + $"\ntrackingState: {trackedImage.trackingState}"
+      + $"\nGUID: {trackedImage.referenceImage.guid}"
+      + $"\nReference size: {trackedImage.referenceImage.size * 100f} cm"
+      + $"\nDetected size: {trackedImage.size * 100f} cm";
+
     var planeParentGo = trackedImage.transform.GetChild(0).gameObject;
     var planeGo = planeParentGo.transform.GetChild(0).gameObject;
-  
+
     // Disable the visual plane if it is not being tracked
     if (trackedImage.trackingState != TrackingState.None)
     {
       planeGo.SetActive(true);
-  
+
       // The image extents is only valid when the image is being tracked
       trackedImage.transform.localScale = new Vector3(trackedImage.size.x, 1f, trackedImage.size.y);
-  
+
+      // store center point for line renderer
+      points.Add(trackedImage.transform.position);
+
       // Set the texture
       var material = planeGo.GetComponentInChildren<MeshRenderer>().material;
       material.mainTexture = (trackedImage.referenceImage.texture == null) ? defaultTexture : trackedImage.referenceImage.texture;
@@ -77,18 +85,23 @@ public class TrackedImageInfoManager : MonoBehaviour
       planeGo.SetActive(false);
     }
   }
-  //
-  void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+
+  private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
   {
+    points = new List<Vector3>();
+
     foreach (var trackedImage in eventArgs.added)
     {
       // Give the initial image a reasonable default scale
       trackedImage.transform.localScale = new Vector3(0.01f, 1f, 0.01f);
-  
+
       UpdateInfo(trackedImage);
     }
-  
+
     foreach (var trackedImage in eventArgs.updated)
       UpdateInfo(trackedImage);
+
+    if (_line != null )
+      _line.SetPositions(points.ToArray());
   }
 }
